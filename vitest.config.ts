32d8391c7a56d10476289db6f25541578eeb749d
Marián Hollý <97178172+MarianHolly@users.noreply.github.com>
2026-03-09 20:23:49 +0100

    Configure vitest testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
   test: {
-    // Vitest configuration options
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.test.ts'],
   },
 });
